e4cd911212becf29730d94d2cc35bb01d97c44b7
WifekRaissi <wifek.raissi@esprit.tn>
2018-02-23 11:43:12 +0100

    v2

--- src/main/kotlin/com/spring/springkotlinmysql/entities/User.kt
+++ src/main/kotlin/com/spring/springkotlinmysql/entities/User.kt
@@ -1,4 +1,4 @@
-package com.spring.springkotlinmysql
+package com.spring.springkotlinmysql.entities
 
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
--- src/main/kotlin/com/spring/springkotlinmysql/repositories/UserRepository.kt
+++ src/main/kotlin/com/spring/springkotlinmysql/repositories/UserRepository.kt
@@ -1,5 +1,6 @@
-package com.spring.springkotlinmysql
+package com.spring.springkotlinmysql.repositories
 
+import com.spring.springkotlinmysql.entities.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
